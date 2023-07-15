5a11917b0dd6a9851891c7dad018243ee6d86042
jottanovaes <jottanovaes@gmail.com>
2023-07-14 23:01:51 -0300

    chore: change tailwindcss version

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+    <div className="p-6 h-screen bg-zinc-950 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )
