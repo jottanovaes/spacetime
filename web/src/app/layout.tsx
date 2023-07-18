824ddf19f60c07399eda0ad5dea7d9972cee0a1f
jottanovaes <jottanovaes@gmail.com>
2023-07-17 23:35:36 -0300

    feat: web layout and authentication

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,23 +1,57 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Roboto_Flex, Bai_Jamjuree } from 'next/font/google'
+import type { Metadata } from "next";
+import { cookies } from "next/headers";
+import { Roboto_Flex, Bai_Jamjuree } from "next/font/google";
 
-const roboto = Roboto_Flex({subsets: ['latin'], variable: '--font-roboto'})
-const bai = Bai_Jamjuree({subsets: ['latin'], weight: '700', variable: '--font-bai'})
+import { Hero } from "@/components/Hero";
+import { Signin } from "@/components/Signin";
+import { Profile } from "@/components/Profile";
+import { Copyright } from "@/components/Copyright";
+
+import "./globals.css";
+const roboto = Roboto_Flex({ subsets: ["latin"], variable: "--font-roboto" });
+const bai = Bai_Jamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai",
+});
 
 export const metadata: Metadata = {
-  title: 'NLW Spacetime',
-  description: 'Uma cápsula do tempo construída com React, Next.js, Typescript e TailwindCSS',
-}
+  title: "NLW Spacetime",
+  description:
+    "Uma cápsula do tempo construída com React, Next.js, Typescript e TailwindCSS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const isAuthenticated = cookies().has("token");
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${bai.variable} font-sans text-gray-100 bg-gray-900`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${bai.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full " />
+
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <Signin />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,7 +1,5 @@
 import { getUser } from "@/lib/auth";
 import Image from "next/image";
-import Link from "next/link";
-
 
 export function Profile() {
   const { name, avatarUrl } = getUser()
@@ -12,7 +10,7 @@ export function Profile() {
       <Image src={avatarUrl} width={40} height={40} alt="" className="h-10 w-10 rounded-full" />
       <p className="max-w-[140px] text-sm leading-snug">
         {name}
-      <Link href="" className=" block text-red-400 hover:text-red-300">Quero sair</Link>
+      <a href="/api/auth/logout" className=" block text-red-400 hover:text-red-300">Quero sair</a>
       </p>
     </div>
   );
