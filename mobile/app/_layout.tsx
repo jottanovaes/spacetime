b7874deae5a114928dece5b32f2badd433c51f20
jottanovaes <jottanovaes@gmail.com>
2023-07-19 23:01:29 -0300

    feat: set initial route

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,59 @@
+import { styled } from 'nativewind'
+import { useEffect, useState } from 'react'
+import { ImageBackground } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+
+import blur_bg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+const StyledStripes = styled(Stripes)
+
+export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
+  const [hasFontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) =>
+      setIsUserAuthenticated(!!token),
+    )
+  }, [])
+
+  if (!hasFontsLoaded) return <SplashScreen />
+
+  return (
+    <ImageBackground
+      className="relative flex-1  bg-gray-900 "
+      source={blur_bg}
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+      <StatusBar style="light" translucent />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: 'transparent' },
+        }}
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="memories" />
+      </Stack>
+    </ImageBackground>
+  )
+}
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,23 +1,12 @@
 import { useEffect } from 'react'
-import { styled } from 'nativewind'
 import { StatusBar } from 'expo-status-bar'
 import * as SecureStore from 'expo-secure-store'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 import { api } from '../src/lib/api'
-import blur_bg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { useRouter } from 'expo-router'
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -28,11 +17,6 @@ const discovery = {
 
 export default function App() {
   const router = useRouter()
-  const [hasFontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
   const [, response, signinWithGitHub] = useAuthRequest(
     {
       clientId: 'f6f73a3cb7e25c003297',
@@ -43,6 +27,7 @@ export default function App() {
     },
     discovery,
   )
+
   async function handleGitHubOAuthCode(code: string) {
     const response = await api.post('/register', {
       code,
@@ -60,16 +45,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasFontsLoaded) return null
-
   return (
-    <ImageBackground
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      source={blur_bg}
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className=" flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
@@ -99,6 +76,6 @@ export default function App() {
         Feito com 💜 no NLW da Rocketseat
       </Text>
       <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   )
 }
