569ce956c9e31752d3816c2b237d0362e294c929
jottanovaes <jottanovaes@gmail.com>
2023-07-20 22:59:23 -0300

    feat: create new memory page

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,9 +1,71 @@
-import { Text, View } from 'react-native'
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link } from 'expo-router'
+import Icon from '@expo/vector-icons/Feather'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Switch } from 'react-native-gesture-handler'
+import React, { useState } from 'react'
 
 export default function NewMemories() {
+  const { bottom, top } = useSafeAreaInsets()
+  const [isPublic, setIsPublic] = useState(false)
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#fff" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            onValueChange={setIsPublic}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+            trackColor={{ false: '#767577', true: '#372560' }}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memória pública
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          <View className="flex-row items-center gap-2">
+            <Icon name="image" color="#fff" />
+            <Text className="font-body text-sm text-gray-200">
+              Adicionar foto ou video de capa
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TextInput
+          multiline
+          className="p-0 font-body text-lg text-gray-50"
+          placeholder="Fique livre para adicionar fotos, videos e relatos sobre essa experiência que vocẽ quer lembrar para sempre"
+          placeholderTextColor="#56565a"
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="items-center rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   )
 }
