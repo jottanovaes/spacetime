428edbd1818d150fb3f1dd56005dc8c66ad8753d
jottanovaes <jottanovaes@gmail.com>
2023-07-20 23:39:10 -0300

    feat: create upload route and static route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,19 +3,24 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import fastifyStatic from '@fastify/static'
+import { resolve } from 'path'
 
 const app = fastify()
 
-app.register(cors, {
-  origin: true,
-})
-
-app.register(jwt, {
-  secret: 'spacetime',
+app.register(cors, { origin: true })
+app.register(jwt, { secret: 'spacetime' })
+app.register(multipart)
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
 })
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
