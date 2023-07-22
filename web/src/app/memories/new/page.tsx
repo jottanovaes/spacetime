a54109aa664ff0db6553c9bc0582c68e8d6425d8
jottanovaes <jottanovaes@gmail.com>
2023-07-21 21:50:30 -0300

    feat: media preview

--- web/src/app/memories/new/page.tsx
+++ web/src/app/memories/new/page.tsx
@@ -1,3 +1,4 @@
+import { MediaPicker } from "@/components/MediaPicker";
 import { Camera, ChevronLeft } from "lucide-react";
 import Link from "next/link";
 
@@ -36,7 +37,9 @@ export default function NewComponent() {
             Tornar memória pública
           </label>
         </div>
-        <input type="file" name="" id="media" className="invisible h-0 w-0" />
+
+        <MediaPicker />
+
         <textarea
           name="content"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
---
+++ web/src/components/MediaPicker.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState< string | null>(null);
+  function onMediaSelected(event: ChangeEvent<HTMLInputElement>) {
+    const {files} = event.target
+
+    if (!files) return 
+    const previewURL = URL.createObjectURL(files[0])
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+      type="file"
+      name=""
+      id="media"
+      className="invisible h-0 w-0"
+      accept="image/*"
+      onChange={onMediaSelected}
+    />
+    {
+      preview && <img src={preview} alt="" className="w-full aspect-video rounded-lg object-cover"/>
+    }
+    </>
+  );
+}
